54c8a7312b27020df0049f56421337f1a6a5f688
Deividas <deivis124@gmail.com>
2021-01-02 16:35:02 +0200

    Creates reaction timer in Block component and logs user time to the console

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">
+  <div class="block" v-if="showBlock" @click="stopTimer">
     click me
   </div>
 </template>
@@ -10,12 +10,15 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
     console.log("component mounted");
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
       console.log(this.delay);
     }, this.delay);
   },
@@ -24,6 +27,17 @@ export default {
   },
   unmounted() {
     console.log("unmounted");
+  },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+    },
   },
 };
 </script>
